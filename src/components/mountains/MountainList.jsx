582330f740cb28628a3398dea2f4205a198ff2fb
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2024-10-30 21:26:51 +0700

    FEAT: list mountain

--- src/components/mountains/MountainList.jsx
+++ src/components/mountains/MountainList.jsx
@@ -1,5 +1,99 @@
+import { useDispatch } from "react-redux";
+import {
+  deleteMountain,
+  updateMountain,
+} from "../../redux/feature/mountainSlice";
+
 const MountainList = () => {
-  return <div></div>;
+  //   const mountains = useSelector((state) => state.mountain.mountains);
+  const dispatch = useDispatch();
+  const handleDelete = (id) => {
+    dispatch(deleteMountain(id));
+  };
+  const handleUpdate = (newMountain) => {
+    dispatch(updateMountain(newMountain));
+  };
+
+  const prototypeMountains = [
+    {
+      id: 1,
+      name: "Mount Fuji",
+      location: "Japan",
+      description: "The highest mountain in Japan",
+      image: "https://source.unsplash.com/random",
+      isConservation: true,
+      pointsOfInterest: "Mount Fuji, Fuji, Japan",
+    },
+    {
+      id: 2,
+      name: "Mount Everest",
+      location: "Nepal",
+      description: "The highest mountain in Nepal",
+      image: "https://source.unsplash.com/random",
+      isConservation: true,
+      pointsOfInterest: "Mount Everest, Everest, Nepal",
+    },
+    {
+      id: 3,
+      name: "Mount everest",
+      location: "Nepal",
+      description: "The highest mountain in Nepal",
+      image: "https://source.unsplash.com/random",
+      isConservation: true,
+      pointsOfInterest: "Mount everest, everest, Nepal",
+    },
+    {
+      id: 4,
+      name: "Mount EVEREST",
+      location: "Nepal",
+      description: "The highest mountain in Nepal",
+      image: "https://source.unsplash.com/random",
+      isConservation: true,
+      pointsOfInterest: "Mount everest, everest, Nepal",
+    },
+    {
+      id: 5,
+      name: "Mount everest",
+      location: "Nepal",
+      description: "The highest mountain in Nepal",
+      image: "https://source.unsplash.com/random",
+      isConservation: true,
+      pointsOfInterest: "Mount everest, everest, Nepal",
+    },
+    {
+      id: 6,
+      name: "Mount everest",
+      location: "Nepal",
+      description: "The highest mountain in Nepal",
+      image: "https://source.unsplash.com/random",
+      isConservation: true,
+      pointsOfInterest: "Mount everest, everest, Nepal",
+    },
+  ];
+  return (
+    <section>
+      {prototypeMountains.map((mountain) => (
+        <section key={mountain.id} className="flex gap-4 justify-between">
+          <section className="flex justify-between">
+            <p>{mountain.name}</p>
+            <p>{mountain.location}</p>
+          </section>
+          <section className="buttonGroup">
+            <button
+              onClick={() => handleDelete(mountain.id)}
+              className="text-neutral-50  bg-successful hover:bg-successfulHover font-bold">
+              Delete
+            </button>
+            <button
+              className="text-neutral-50  bg-successful hover:bg-successfulHover font-bold"
+              onClick={() => handleUpdate(mountain)}>
+              Update
+            </button>
+          </section>
+        </section>
+      ))}
+    </section>
+  );
 };
 
 export default MountainList;
--- src/pages/mountain/Mountain.jsx
+++ src/pages/mountain/Mountain.jsx
@@ -1,12 +1,49 @@
 import { useSelector } from "react-redux";
 import FormMountain from "../../components/mountains/FormMountain";
+import MountainList from "../../components/mountains/MountainList";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+import { useState } from "react";
 
 const Mountain = () => {
-  const mountains = useSelector((state) => state.mountain.mountains);
+  const mountains = useSelector((state) => state.mountain.mountains || []);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure(false);
+
   return (
     <section className="w-96 font-inter">
       <h1>Mountain Management</h1>
-      <FormMountain />
+      <Button onPress={onOpen}>Add Mountain</Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(closeModal) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
+              <ModalBody>
+                <FormMountain onClose={closeModal} />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={closeModal}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={closeModal}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <MountainList />
     </section>
   );
 };
